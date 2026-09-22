a6a76ca4c166f6fe7e1e9aaebcc3af8e4860c15b
jmizzo29 <webbywiz@gmail.com>
2026-09-22 15:02:23 -0700

    Pretty phone Sign in: header pill and a flush dark door.

--- app/signin/[[...sign-in]]/page.tsx
+++ app/signin/[[...sign-in]]/page.tsx
@@ -33,6 +33,7 @@ export default async function SignInPage() {
     <PublicChrome auth>
       <AuthDoor
         title={SIGN_IN_H1}
+        lead="Back to your hunts. You still approve the money."
         foot={
           <p className="bb-auth-alt">
             New here? <Link href={CLERK_SIGN_UP_URL}>Sign up</Link>
